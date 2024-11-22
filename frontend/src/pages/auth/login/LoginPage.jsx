40542ff834ff47158450b17f1799ddd03c590069
Raidanor <silverstallion30@gmail.com>
2024-11-21 23:36:47 -0400

    added login and signup pages

--- frontend/src/pages/auth/login/LoginPage.jsx
+++ frontend/src/pages/auth/login/LoginPage.jsx
@@ -1,10 +1,77 @@
+import { useState } from "react"
+import { Link } from "react-router-dom"
+
+import XSvg from "../../../components/X"
+
+import { MdOutlineMail } from "react-icons/md"
+import { MdPassword } from "react-icons/md"
+
 const LoginPage = () =>
+{
+    const [formData, setFormData] = useState({
+        username: "",
+        passowrd: ""
+    })
+
+    const handleSubmit = (e) =>
     {
-        return(
-            <div>
-                Login page
-            </div>
-        )
+        e.preventDefault()
+        console.log(formData)
     }
+
+    const handleInputChange = (e) =>
+    {
+        setFormData({ ...formData, [e.target.name]: e.target.value})
+    }
+
+    const isError = false
     
-    export default LoginPage
+
+    return(
+        <div className="max-w-screen-xl mx-auto flex h-screen px-10">
+            {/* <div className="flex-1 hidden lg:flex flex-col justify-center items-center">
+                <XSvg className="lg:w-2/3 fill-white"/>
+            </div> */}
+            <div className="flex-1 flex flex-col justify-center items-center">
+                <form className="lg:w-2/3 mx-auto md:mx-20 flex gap-4 flex-col" onSubmit={handleSubmit}>
+                    <XSvg className="lg:w-2/3 fill-white"/>
+                    {/* <XSvg className="w-24 lg:hidden fill-white" /> */}
+                    <h1 className="text-4xl font-extrabold text-white">{"Let's"} go</h1>
+
+                    <label className="input input-bordered ronded flex items-center gap-2">
+                        <MdOutlineMail />
+                        <input
+                            type="text"
+                            className="grow"
+                            placeholder="username"
+                            onChange={handleInputChange}
+                            value={formData.username}
+                        />
+                    </label>
+
+                    <label className="input input-bordered ronded flex items-center gap-2">
+                        <MdPassword />
+                        <input
+                            type="text"
+                            className="grow"
+                            placeholder="username"
+                            onChange={handleInputChange}
+                            value={formData.username}
+                        />
+                    </label>
+
+                    <button className="btn rounded-full btn-primary text-white btn-outline w-full">Login</button>
+                    { isError && <p className="text-red-500">Something went wrong</p>}
+                </form>
+                <div className="flex flex-col lg:w-2/3 gap-2 mt-4">
+                    <p className="text-white text-lg">{"Don't"} have an account?</p>
+                    <Link to="/signup">
+                        <button className="btn rounded-full btn-primary text-white btn-outline w-full">Create an account</button>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    )
+}
+    
+export default LoginPage
