ff351d0cb7aeb3fa88c41aef381a217f4db23b9c
Raidanor <silverstallion30@gmail.com>
2024-12-04 15:53:37 -0400

    deployment configuration

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from "express"
+import path from "path"
 
 import dotenv from "dotenv"
 import { v2 as cloudinary } from "cloudinary"
@@ -25,6 +26,8 @@ cloudinary.config({
 
 const PORT = process.env.PORT || 5000
 
+const __dirname = path.resolve()
+
 app.use(express.json({ limit: "5mb" }))
 app.use(cookieParser()) // for jwt cookies
 
@@ -36,7 +39,14 @@ app.use("/api/users", userRoutes)
 app.use("/api/post", postRoutes)
 app.use("/api/notifications", notificationRoutes)
 
+if (process.env.NODE_ENV === "production")
+{
+    app.use(express.static(path.join(__dirname, "/frontend/dist")))
 
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "fronend", "dist", "index.html"))
+    })
+}
 
 app.listen(PORT, () =>
 {
--- frontend/src/hooks/useUpdateUserProfile.jsx
+++ frontend/src/hooks/useUpdateUserProfile.jsx
@@ -1,41 +1,40 @@
 import toast from "react-hot-toast"
 import { useQueryClient, useMutation } from "@tanstack/react-query"
 
-const useUpdateUserProfile = () => {
-	const queryClient = useQueryClient()
+const useUpdateUserProfile = () =>
+{
+    const queryClient = useQueryClient()
 
-	const { mutateAsync: updateProfile, isPending: isUpdatingProfile } = useMutation({
-		mutationFn: async (formData) => {
-			try {
-				const res = await fetch(`/api/users/update`, {
+    const {mutateAsync:updateProfile, isPending:isUpdatingProfile} = useMutation({
+        mutationFn:async(formData) => {
+            try {
+                const res = await fetch(`/api/users/update`, {
 					method: "POST",
 					headers: {
 						"Content-Type": "application/json",
 					},
 					body: JSON.stringify(formData),
 				})
-				const data = await res.json()
+				const data = await res.json();
 				if (!res.ok) {
 					throw new Error(data.error || "Something went wrong")
 				}
 				return data
-			} catch (error) {
-				throw new Error(error.message)
-			}
-		},
-		onSuccess: () => {
-			toast.success("Profile updated successfully")
-			Promise.all([
-				queryClient.invalidateQueries({ queryKey: ["authUser"] }),
-				queryClient.invalidateQueries({ queryKey: ["userProfile"] }),
-			])
-		},
-		onError: (error) => {
-			toast.error(error.message)
-		},
-	})
+            } catch (error) {
+                throw new Error(error.message)
+            }
+        }, onSuccess:() => {
+            toast.success("Profile updated")
+            Promise.all([
+                queryClient.invalidateQueries({queryKey: ["authUser"]}),
+                queryClient.invalidateQueries({queryKey: ["userProfile"]}),
+            ])
+        }, onError:(error) => {
+            toast.error(error.message)
+        }
+    })
 
-	return { updateProfile, isUpdatingProfile }
+    return {updateProfile, isUpdatingProfile}
 }
 
 export  default useUpdateUserProfile
